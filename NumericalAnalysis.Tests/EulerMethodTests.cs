0cbc94ea631f4952a5294a0315f1daa7350f290a
Tokarev Maxim <nepfion96@gmail.com>
2017-03-09 01:27:43 +0300

    Lab1 complete

---
+++ NumericalAnalysis.Tests/EulerMethodTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NumericalAnalysis;
+
+namespace NumericalAnalysis.Tests
+{
+    [TestClass]
+    public class EulerMethodTests
+    {
+        [TestMethod]
+        public void SolveExplicit_Compare_With_Example()
+        {
+            //Arrange
+            Equation func = delegate (double x, double[] y)
+            {
+                return -y[0] + x * x + 2 * x + 2;
+            };
+            ODE[] ode = new ODE[] { new ODE(func, 0, 2) };
+
+            double a = 0;
+            double b = 1;
+            int N = 4;
+            double h = (b - a) / N;
+            double[][] expected = new double[N + 1][];
+            double step = a;
+            expected[0] = new double[] { 2 };
+            expected[1] = new double[] { 2 };
+            expected[2] = new double[] { 2.140625 };
+            expected[3] = new double[] { 2.41796875 };
+            expected[4] = new double[] { 2.8291015625 };
+
+            //Act
+            double[][] actual = EulerMethod.SolveExplicit(ode, a, b, N);
+
+            //Assert
+            for (int i = 0; i <= N; i++)
+                for (int j = 0; j < ode.Length; j++)
+                    Assert.AreEqual(expected[i][j], actual[i][j]);
+        }
+    }
+}
---
+++ NumericalAnalysis/EulerMethod.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumericalAnalysis
+{
+    public static class EulerMethod
+    {
+        public static double[][] SolveExplicit(ODE[] odes, double a, double b, int N)
+        {
+            //Initialization
+            double[][] result = new double[N + 1][];
+
+            for (int i = 0; i < result.Length; i++)
+                result[i] = new double[odes.Length];
+            for (int i = 0; i < odes.Length; i++)
+                result[0][i] = odes[i].y0;
+
+            //Algorithm
+            double h = (b - a) / N;
+            double x = a;
+
+            for (int i = 1; i < result.Length; i++, x += h)
+                for (int j = 0; j < result[i].Length; j++)
+                {
+                    result[i][j] = result[i - 1][j] + h * odes[j].Equation(x, result[i - 1]);
+                }
+
+            return result;
+        }
+
+        public static double[][] SolveImplicit(ODE[] odes, double a, double b, int N)
+        {
+            //Initilalization
+            double[][] result = new double[N + 1][];
+
+            for (int i = 0; i < result.Length; i++)
+                result[i] = new double[odes.Length];
+            for (int i = 0; i < odes.Length; i++)
+                result[0][i] = odes[i].y0;
+
+            //Algorithm
+            double h = (b - a) / N;
+            double x = a + h;
+
+            for (int i = 1; i < result.Length; i++, x += h)
+            {
+                Matrix invertedJacobian = GetJacobian(odes, x, result[i - 1]).Inversion;
+                Matrix X0 = new Matrix(result[i - 1]);
+                Matrix fX0 = new Matrix(odes.Length, 1);
+
+                for (int j = 0; j < result[i].Length; j++)
+                    fX0[j, 0] = odes[j].Equation(x, result[i - 1]);
+
+                Matrix newton = (X0 - invertedJacobian * fX0);
+                newton.Transpose();
+
+                for (int j = 0; j < result[i].Length; j++)
+                {
+                    result[i][j] = result[i - 1][j] + h * odes[j].Equation(x, newton.JaggedArray[0]);
+                }
+            }
+
+            //throw new NotImplementedException();
+            return result;
+        }
+
+        public static Matrix GetJacobian(ODE[] odes, double x, double[] args)
+        {
+            Matrix result = new Matrix(odes.Length, args.Length);
+
+            for (int i = 0; i < result.Dimension.rowCount; i++)
+                for (int j = 0; j < result.Dimension.columnCount; j++)
+                    result[i, j] = odes[i].Deriatives[j + 1](x, args); //Значение х???
+
+            return result;
+        }
+    }
+}
